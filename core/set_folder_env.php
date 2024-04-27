6a24c4163bfb5090bd3fbcf7440c22bc1dbb4e4c
Tanguy Gibrat <twnguy@macbook-pro-de-tanguy.home>
2024-04-27 10:57:24 +0200

    fix script install

--- core/set_folder_env.php
+++ core/set_folder_env.php
@@ -3,7 +3,7 @@ $projectName = isset($argv[1]) ? $argv[1] : '';
 
 if (!empty($projectName)) {
     $envContent = "ENV_FOLDER=$projectName\n";
-    $envFile = __DIR__ . '/config.env';
+    $envFile = __DIR__ . '/../config.env';
 
     file_put_contents($envFile, $envContent, FILE_APPEND | LOCK_EX);
 }
